5f2c5f53102dcea70cc0c2ae128dea8056cd810d
Laryssa Peçanha <laryssascampark@gmail.com>
2023-07-22 12:21:02 -0300

    classe principal escola com array de turmas

--- Projetos Estudos Java/src/GerenciadorEscolar/Cria_turma.java
+++ Projetos Estudos Java/src/GerenciadorEscolar/Cria_turma.java
@@ -1,7 +1,7 @@
 package GerenciadorEscolar;
 
 import java.util.Scanner;
-public class Cria_aluno {
+public class Cria_turma {
     public String aluno;
     public String getAluno() { return aluno; }
     public void setAluno(String aluno) { this.aluno = aluno; }
--- Projetos Estudos Java/src/GerenciadorEscolar/Escola.java
+++ Projetos Estudos Java/src/GerenciadorEscolar/Escola.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Escola {
     public static void main(String [] args) {
-        final String[] turma = {"Naruto","Sakura","Sasuke"};
+        Cria_turma cria = new Cria_turma();
+
+        final String[] Escola_turma = {"Turma-1","Turma-2","Turma-3"};
+
         Scanner ler = new Scanner(System.in);
     }
 }
