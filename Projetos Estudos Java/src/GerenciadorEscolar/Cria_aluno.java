93201b0112eabbc1efb495a165580a432066e432
Laryssa Peçanha <laryssascampark@gmail.com>
2023-07-22 12:07:14 -0300

    retaforação do código

---
+++ Projetos Estudos Java/src/GerenciadorEscolar/Cria_aluno.java
@@ -0,0 +1,13 @@
+package GerenciadorEscolar;
+
+import java.util.Scanner;
+public class Cria_aluno {
+    public String aluno;
+    public String getAluno() { return aluno; }
+    public void setAluno(String aluno) { this.aluno = aluno; }
+    public static void main (String [] args ) {
+        final String[] Alunos = {"Naruto","Sakura","Sasuke"};
+        Scanner ler = new Scanner(System.in);
+    }
+
+}
---
+++ Projetos Estudos Java/src/GerenciadorEscolar/Escola.java
@@ -0,0 +1,10 @@
+package GerenciadorEscolar;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Escola {
+    public static void main(String [] args) {
+        final String[] turma = {"Naruto","Sakura","Sasuke"};
+        Scanner ler = new Scanner(System.in);
+    }
+}
